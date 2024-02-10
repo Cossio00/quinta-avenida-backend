686938a9d34dc0f46ddb077cbf2ea0fd165d114b
Vinícius Cossio de Oliveira <vinicossio@gmail.com>
2024-02-09 22:23:51 -0300

    added database config files

---
+++ src/dbConfig/configDB.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "rootadminpassword",
+        database: "quinta_avenida_pastelaria"
+    }
+
+  };
+  
+  module.exports = config;
