d090a4d81631d04f83897f23c1c5823f0ccc8df0
Vinícius Cossio de Oliveira <vinicossio@gmail.com>
2024-02-09 22:00:42 -0300

    added app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = 3010;
+const httpsPort = 3011;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`A API está rodando na porta: ${port}`)
+})
